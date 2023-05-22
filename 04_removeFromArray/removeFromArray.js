76fe0a0a07fa1a278e8368959fca60639e9a2fac
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-05-22 22:36:20 +0200

    add a splice function to remove the number 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...args) {
+    if (args == 3) {
+        array1.splice(2, 1);
+        return array1;
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
