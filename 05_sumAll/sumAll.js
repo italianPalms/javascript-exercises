818e03de293831b916d3a23096e89c52ca5cde9a
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-05-23 23:46:48 +0200

    add function to sum all and run two test step

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+
+const sumAll = function(min, max) {
+    let sum = 0;
+    for (let num = min; num <= max; num++) {
+        sum += num;
+    }
+
+    return sum;
 
 };
 
