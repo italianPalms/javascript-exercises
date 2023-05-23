f4f7a02787a9fd28eedeb7b95071137623de405b
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-05-23 23:12:16 +0200

    adjust code to remove multiple values from the original array, and open next test step

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,12 @@
 const removeFromArray = function(array1, ...args) {
-    if (args == 3) {
-        array1.splice(2, 1);
-        return array1;
-    }
-}
-
+    args.forEach((arg) => {
+    const index = array1.indexOf(arg);
+    if (index > -1)
+      array1.splice(index, 1);
+  });
+  return array1;
+    
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
