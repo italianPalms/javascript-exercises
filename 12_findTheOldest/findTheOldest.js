7d3fce0b9d4af9b2ee42abe0a80a2fdfa7bb8418
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-06-14 23:20:53 +0200

    return the oldest person from the array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
 
+
+
+const findTheOldest = function(arr) {
+    let oldestPerson = arr[0]; //Assume the first person is oldest 
+
+    for (let i = 1; i < arr.length; i++) {
+        const currentPerson = arr[i];
+
+        //Calculate the age of the current person
+        const currentAge = calculateAge(currentPerson);
+
+        //Calculate the age of the oldest person
+        const oldestAge = calculateAge(oldestPerson);
+
+        //If the current person's age is greater than the oldest person's age, update the oldestPerson variable. 
+        if (currentAge > oldestAge) {
+            oldestPerson = currentPerson;
+        }
+    }
+
+    return oldestPerson;
 };
+
+//Helper function to calculate the age of a person
+function calculateAge(person) {
+    if (!person.yearOfDeath) {
+        //If yearOfDeath is not provided, assume the person is still alive 
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
