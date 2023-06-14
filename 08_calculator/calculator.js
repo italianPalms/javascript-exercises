3990ec585c0606adc507812e4f36ecd32830f1d2
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-06-14 20:34:36 +0200

    solve all 16 challenged in the calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,16 +14,26 @@ const sum = function(array) {
   return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+return number.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1; 
+
+  for (let i = 0; i < b; i++) {
+    result *= a; 
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+
+  for (let i = 2; i <= n; i++) {
+    result *= i; 
+  }
+  return result;
 };
 
 // Do not edit below this line
