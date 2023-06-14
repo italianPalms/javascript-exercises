bd5656c7d578430e32cbfbc97d06c6fb8d6d500b
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-06-14 21:09:18 +0200

    solve all taks in the palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
 
+
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/\W/g, '');
+    let len = string.length;
+   
+    for (let i = 0; i < len / 2; i++) {
+        if (string[i] !== string[len -1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
